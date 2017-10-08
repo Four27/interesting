d462738e71af6b2049ef9f632dfda29e3703a2c3
Shirley <1733025299@qq.com>
2017-10-08 22:59:20 +0800

    add some test code

--- server/routes/userLogin.js
+++ server/routes/userLogin.js
@@ -1,3 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// router.get('/', function (req, res) {
+//     res.send('Hello world');
+// });
+
+// middleware that is specific to this router
+
+// define the home page route
+router.get('/', function(req, res) {
+  res.send('Birds home page');
+});
+// define the about route
+router.get('/about', function(req, res) {
+  res.send('About birds');
+});
+
+module.exports = router;
