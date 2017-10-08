1739a57fb94568ad62d879c8c1d72712f14f3734
Shirley <1733025299@qq.com>
2017-10-08 22:59:53 +0800

    add some test code

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {   // 中间件函数
+  var req.requestTime = Date.now();
   res.send('respond with a resource');
+  next();
+}, function (req, res) {
+  res.send('respond with a resource once more');
 });
 
 module.exports = router;
