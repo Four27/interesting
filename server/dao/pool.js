f2fd686b00e4cd84a0af3456d2297d2c5517cbbf
Shirley <1733025299@qq.com>
2017-10-12 23:10:50 +0800

    fis the bug

--- server/dao/pool.js
+++ server/dao/pool.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var db = require('./db');
 
-var pool = mysql.createPool(db.mysql);   // 创建数据库连接池
+var pool = mysql.createPool(db.mySql);   // 创建数据库连接池
 
 module.exports = pool;
