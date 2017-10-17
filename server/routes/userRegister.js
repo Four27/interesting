583f5bac3c286b0e07c1a5bcd8528f630926da24
Shirley <1733025299@qq.com>
2017-10-17 23:27:47 +0800

    modified userRegister

--- server/routes/userRegister.js
+++ server/routes/userRegister.js
@@ -8,7 +8,7 @@ router.post('/', function (req, res) {
   pool.getConnection(function (err, connection) {
     if (err) {             // 这里的错误是指数据库未连接成功
       response = {
-        status: 403,
+        status: 500,
         msg: '数据库连接出现错误',
       }
       res.end(JSON.stringify(response));
@@ -16,7 +16,7 @@ router.post('/', function (req, res) {
       connection.query(userSQL.userRegisterInsert, [req.body.userId, req.body.userName, req.body.userPwd], function (err, result) {
         if (err) {
           response = {
-            status: 403,
+            status: 500,
             msg: '插入数据失败！'
           }
           res.end(JSON.stringify(response));
