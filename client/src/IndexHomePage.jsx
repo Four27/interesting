d71a3acb2239b67578d405d2198010da45a543aa
Shirley <1733025299@qq.com>
2017-10-22 23:22:11 +0800

    test react-router

---
+++ client/src/IndexHomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class IndexHeader extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>App Home page</h2>
+            </div>
+        ) 
+    }
+}
+
+export default IndexHeader;
