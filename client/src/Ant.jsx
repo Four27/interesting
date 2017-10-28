4f954cf137a3d8b2265462c9e532685a33e0f256
Shirley <1733025299@qq.com>
2017-10-28 23:06:54 +0800

    test ant-design

---
+++ client/src/Ant.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+import './Ant.css';
+
+class Ant extends React.Component {
+    render() {
+        return (
+            <div className = 'Ant'>
+                <Button type = 'primary'>Button</Button>
+            </div>
+        )
+    }
+} 
+
+export default Ant;
